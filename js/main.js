b6f5506af8fbf7e4b13cbb0709e97e42a0f32c8a
Gabriel <gabrielbarbosa.j01@gmail.com>
2023-01-21 20:54:03 -0300

    Armazenando Dados do Forms

--- js/main.js
+++ js/main.js
@@ -1,25 +1,40 @@
-const  form = document.getElementById("novoItem")
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = []
 
-form.addEventListener("submit", (evento)=>{
-    evento.preventDefault();
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
 
-criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
 
+    criaElemento(nome.value, quantidade.value)
+    
+    // Deixa os formulários vazios
+    nome.value = ""
+    quantidade.value = ""
 })
 
-function criaElemento(nome, quantidade){
-    // Criando um elemento lista
+function criaElemento(nome, quantidade) {
+     // Criando um elemento lista
     const novoItem = document.createElement('li')
     // Adicionando a classe no elemento criado 
     novoItem.classList.add("item")
     // Criando um elemento strong
-    const numeroItem =document.createElement('strong')
+    const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = quantidade
-
     novoItem.appendChild(numeroItem)
+
     novoItem.innerHTML += nome
 
-    const lista = document.getElementById('lista')
-
     lista.appendChild(novoItem)
-} 
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("item", JSON.stringify(itens))
+}
