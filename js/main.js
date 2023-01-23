f5af399e0b4c8cfc8127fddfc0a4aa7bb33ef636
Gabriel <gabrielbarbosa.j01@gmail.com>
2023-01-23 10:55:08 -0300

    Atualização de Itens Dinamicamente

--- js/main.js
+++ js/main.js
@@ -1,54 +1,74 @@
-// Armazena o elemento do formulário com id 'novoItem' em uma constante
-const form = document.getElementById("novoItem") 
-// Armazena o elemento da lista com id 'lista' em uma constante
+// Recupera o formulário e a lista do HTML
+const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
-// Armazena os itens armazenados no localStorage com a chave 'itens', convertendo-os de string para objeto JavaScript ou inicializa como um array vazio
-const itens = JSON.parse(localStorage.getItem("itens")) || []  
 
-// Itera sobre os itens e chama a função 'criaElemento' para cada um
-itens.forEach( (elemento) => {    
+// Recupera os itens salvos no LocalStorage ou cria um array vazio
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+// Exibe os itens salvos na tela
+itens.forEach( (elemento) => {
     criaElemento(elemento)
 } )
 
-// Adiciona um evento de submit ao formulário, chamando a função anônima
-form.addEventListener("submit", (evento) => {  
-    // Previne o comportamento padrão do formulário de recarregar a página
+// Adiciona um evento de submit ao formulário para adicionar novos itens à lista
+form.addEventListener("submit", (evento) => {
+    // Previne o comportamento padrão de submissão do formulário
     evento.preventDefault()
 
-    // Armazena os elementos 'nome' e 'quantidade' do formulário em constantes
+    // Recupera os valores de nome e quantidade do formulário
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    // Cria um objeto com as propriedades 'nome' e 'quantidade'
+    // Verifica se o item já existe na lista
+    const existe = itens.find( elemento => elemento.nome === nome.value )
+
+    // Cria um objeto com os dados do item atual
     const itemAtual = {
-    "nome": nome.value,
-    "quantidade": quantidade.value
+        "nome": nome.value,
+        "quantidade": quantidade.value
     }
 
-    // Chama a função 'criaElemento' passando o itemAtual como argumento
-    criaElemento(itemAtual)
+    // Se o item já existe, atualiza a quantidade e o LocalStorage
+    if (existe) {
+        itemAtual.id = existe.id
+        
+        atualizaElemento(itemAtual)
 
-    // Adiciona o itemAtual ao array itens
-    itens.push(itemAtual)
+        itens[existe.id] = itemAtual
+    } else {
+        // Senão, adiciona o item à lista e ao LocalStorage
+        itemAtual.id = itens.length
 
-    // Armazena o array itens no localStorage, convertendo-o de objeto JavaScript para string
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
+
+    // Atualiza o LocalStorage com a lista atualizada
     localStorage.setItem("itens", JSON.stringify(itens))
 
-    // Limpa os campos 'nome' e 'quantidade' do formulário
+    // Limpa os campos do formulário
     nome.value = ""
     quantidade.value = ""
 })
 
-// Função que cria um elemento 'li' com as propriedades do objeto 'item' e o adiciona à lista
-function criaElemento(item) {  
-    const novoItem = document.createElement('li')
+// Função para criar um novo elemento na tela representando o item
+function criaElemento(item) {
+    // Cria um novo elemento "li"
+    const novoItem = document.createElement("li")
     novoItem.classList.add("item")
 
-    const numeroItem = document.createElement('strong')
+    // Cria um elemento "strong" para exibir a quantidade do item
+    const numeroItem = document.createElement("strong")
     numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
-
+    
+    // Adiciona o nome do item ao elemento "li"
     novoItem.innerHTML += item.nome
 
+    // Adiciona o novo item à lista
     lista.appendChild(novoItem)
 }
+
+// Função para at
